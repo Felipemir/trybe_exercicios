d44c4febd4407bac51e407303c04d2b39be58356
Felipemir <robsonfelipeprog@gmail.com>
2023-02-27 20:06:52 -0400

    fixar 02

--- fundamentos/dia-01-html-css/javascript/fixar01.js
+++ fundamentos/dia-01-html-css/javascript/fixar01.js
@@ -2,4 +2,4 @@ let myName = "Felipe";
 let birthCity ="Manaus";
 let birthYear = 2002;
 
-console.log(myName, birthCity, birthYear);
+console.log(myName, birthCity, birthYear);b  
---
+++ fundamentos/dia-01-html-css/javascript/fixar02.js
@@ -0,0 +1,8 @@
+let base =5;
+let heigth = 8;
+let area = base*heigth;
+let perimeter = base + base + heigth + heigth;
+
+
+
+console.log("multiplacação",area, "soma", perimeter);
