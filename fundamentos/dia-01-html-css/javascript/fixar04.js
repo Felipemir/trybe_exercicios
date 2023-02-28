d26b37d931163fb9ac3bf82cfedef9874fc10449
Felipemir <robsonfelipeprog@gmail.com>
2023-02-28 14:25:11 -0400

    fixar 04 uso de if e else

---
+++ fundamentos/dia-01-html-css/javascript/fixar04.js
@@ -0,0 +1,15 @@
+let currentHour = 5;
+let message = "";
+
+if(currentHour >= 22){
+    console.log( message ="não comer nada");
+}else if(currentHour >= 18 && currentHour < 22){
+    console.log(message = "rango da noite");
+}else if(currentHour >= 14 && currentHour < 18){
+    console.log(message = "café da tarde");
+}else if(currentHour >= 11 && currentHour < 14){
+    console.log(message = "almoço");
+}else if( currentHour >= 4 && currentHour < 11){
+    console.log(message="café bem passado");
+}
+
