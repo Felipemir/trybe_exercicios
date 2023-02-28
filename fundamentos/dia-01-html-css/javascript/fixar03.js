f3c8798b4d3b55ffeba167444876887aed6b01c1
Felipemir <robsonfelipeprog@gmail.com>
2023-02-27 21:30:30 -0400

    fixar 03

---
+++ fundamentos/dia-01-html-css/javascript/fixar03.js
@@ -0,0 +1,11 @@
+let nota = 10;
+
+if(nota >= 80){
+    console.log("Aprovada!");
+} else if(nota < 80 && nota == 60){
+    console.log("lista de espera");
+}else if(nota < 60){
+    console.log("reprovado !");
+}
+
+console.log(nota);
